e8dd111b4977c08c55c2c51f1944371edaa894db
Shalini Priya <47511430+sptravi@users.noreply.github.com>
2023-05-26 16:35:29 +0530

    Update fulladder.v

--- fulladder.v
+++ fulladder.v
@@ -5,13 +5,4 @@
                       output [3:0] sum);  
       
        assign {c_out, sum} = a + b + c_in;  
-    endmodule      module fulladder (  input [3:0] a,  
-                      input [3:0] b,  
-                      input c_in,  
-                      output c_out,  
-                      output [3:0] sum);  
-      
-       assign {c_out, sum} = a + b + c_in;  
-    endmodule  
-
-
+    endmodule     
